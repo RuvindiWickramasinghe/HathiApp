0dc2ea56da619490a74bea6e09d5dcc1e9365138
Nelith Karunarathne <nelith550@gmail.com>
2024-03-06 00:58:39 +0530

    Fixed buyer_home and menu bar
    added names under all images and fixed the menu bar a bit by adding icons.

--- lib/buyer_home.dart
+++ lib/buyer_home.dart
@@ -34,7 +34,9 @@ class _nelithState extends State<nelith> {
             ),
         ],
       ),
+
 //this is the home//
+
       drawer: Drawer(
         backgroundColor: Color.fromRGBO(255, 138, 0, 1),
         child: ListView(
@@ -42,11 +44,13 @@ class _nelithState extends State<nelith> {
             ListTile(
               titleTextStyle: TextStyle(fontSize: 28,fontWeight: FontWeight.bold,),
               textColor: Colors.white,
+              trailing: Icon(Icons.arrow_back),iconColor: Colors.white,
               title: Text("MENU",),
             ),
             ListTile(
               titleTextStyle: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),
               textColor: Colors.white,
+              trailing: Icon(Icons.home),iconColor: Colors.white,
               title: Text("HOME"),
               onTap: () {
                 print("cLICKED");
@@ -56,6 +60,7 @@ class _nelithState extends State<nelith> {
               
               titleTextStyle: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),
               textColor: Colors.white,
+              trailing: Icon(Icons.person),iconColor: Colors.white,
               title: Text("Profile"),
               onTap: () {
                 print("cLICKED");
@@ -70,6 +75,9 @@ class _nelithState extends State<nelith> {
       Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children:[ 
+
+//row one//
+
           Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
@@ -120,67 +128,52 @@ class _nelithState extends State<nelith> {
         
             GestureDetector(
               onTap: () {
-                print('Hello');
+               print('Hello');
               },
-              child: Container(
-                child: Image(image: AssetImage('assets/party.png')),
-                  height: 160,
-                  width: 160,
+            child: Row(
+              children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(image: AssetImage('assets/party.png')),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        style:TextStyle(fontWeight: FontWeight.bold),
+                        'Events &\n Entertainment'
+                      ),
+                    ],
+                    ),
+                  ],
+                ),
+                height: 160,
+                width: 160,
                 decoration: BoxDecoration(
                 color: Color.fromRGBO(253, 255, 241, 1),
                 borderRadius: BorderRadius.circular(12),
                 border: Border.all(
-                width: 2,
-                color: Color.fromRGBO(255, 138, 0, 1),
+                  width: 2,
+                  color: Color.fromRGBO(255, 138, 0, 1),
+              ),
+              ),
+
+                      ),],
             ),
-          ),
-        ),
-            ),
-          ],
-        ),
-        
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            GestureDetector(
-              onTap: () {
-               print('Hello');
-              },
-            child: Container(
-            child: Image(image: AssetImage('assets/car.png')),
-            height: 160,
-            width: 160,
-            decoration: BoxDecoration(
-            color: Color.fromRGBO(253, 255, 241, 1),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              width: 2,
-              color: Color.fromRGBO(255, 138, 0, 1),
-            ),
-          ),
-        ),
-            ),
-        
-            GestureDetector(
-              onTap: () {
-                print('Hello');
-              },
-            child: Container(
-              child: Image(image: AssetImage('assets/work.png')),
-                height: 160,
-                width: 160,
-              decoration: BoxDecoration(
-              color: Color.fromRGBO(253, 255, 241, 1),
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
-              width: 2,
-              color: Color.fromRGBO(255, 138, 0, 1),
-            ),
-          ),
-        ),
             ),
           ],
         ),
+
+//row two//
 
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -189,41 +182,193 @@ class _nelithState extends State<nelith> {
               onTap: () {
                print('Hello');
               },
-            child: Container(
-            child: Image(image: AssetImage('assets/garden.png')),
-            height: 160,
-            width: 160,
-            decoration: BoxDecoration(
-            color: Color.fromRGBO(253, 255, 241, 1),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              width: 2,
-              color: Color.fromRGBO(255, 138, 0, 1),
+            child: Row(
+              children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(image: AssetImage('assets/car.png')),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        style:TextStyle(fontWeight: FontWeight.bold),
+                        'Vehicle Services'
+                      ),
+                    ],
+                    ),
+                  ],
+                ),
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                color: Color.fromRGBO(253, 255, 241, 1),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  width: 2,
+                  color: Color.fromRGBO(255, 138, 0, 1),
+              ),
+              ),
+
+                      ),],
             ),
-          ),
-        ),
             ),
         
             GestureDetector(
               onTap: () {
-                print('Hello');
+               print('Hello');
               },
-            child: Container(
-              child: Image(image: AssetImage('assets/health.png')),
+            child: Row(
+              children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(image: AssetImage('assets/work.png')),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        style:TextStyle(fontWeight: FontWeight.bold),
+                        'Electrical Services'
+                      ),
+                    ],
+                    ),
+                  ],
+                ),
                 height: 160,
                 width: 160,
-              decoration: BoxDecoration(
-              color: Color.fromRGBO(253, 255, 241, 1),
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
-              width: 2,
-              color: Color.fromRGBO(255, 138, 0, 1),
+                decoration: BoxDecoration(
+                color: Color.fromRGBO(253, 255, 241, 1),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  width: 2,
+                  color: Color.fromRGBO(255, 138, 0, 1),
+              ),
+              ),
+
+                      ),],
             ),
-          ),
-        ),
             ),
           ],
         ),
+
+
+//row three//
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            GestureDetector(
+              onTap: () {
+               print('Hello');
+              },
+            child: Row(
+              children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(image: AssetImage('assets/garden.png')),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        style:TextStyle(fontWeight: FontWeight.bold),
+                        'Garden Services'
+                      ),
+                    ],
+                    ),
+                  ],
+                ),
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                color: Color.fromRGBO(253, 255, 241, 1),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  width: 2,
+                  color: Color.fromRGBO(255, 138, 0, 1),
+              ),
+              ),
+
+                      ),],
+            ),
+            ),
+        
+            GestureDetector(
+              onTap: () {
+               print('Hello');
+              },
+            child: Row(
+              children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(image: AssetImage('assets/health.png')),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        style:TextStyle(fontWeight: FontWeight.bold),
+                        'Health &\nPhysical services'
+                      ),
+                    ],
+                    ),
+                  ],
+                ),
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                color: Color.fromRGBO(253, 255, 241, 1),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  width: 2,
+                  color: Color.fromRGBO(255, 138, 0, 1),
+              ),
+              ),
+
+                      ),],
+            ),
+            ),
+          ],
+        ),
+
+
+//row four//
 
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -232,19 +377,45 @@ class _nelithState extends State<nelith> {
               onTap: () {
                print('Hello');
               },
-            child: Container(
-            child: Image(image: AssetImage('assets/dots.png')),
-            height: 160,
-            width: 160,
-            decoration: BoxDecoration(
-            color: Color.fromRGBO(253, 255, 241, 1),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              width: 2,
-              color: Color.fromRGBO(255, 138, 0, 1),
+            child: Row(
+              children: [
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(image: AssetImage('assets/dots.png'),width: 70,),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        style:TextStyle(fontWeight: FontWeight.bold),
+                        'Other'
+                      ),
+                    ],
+                    ),
+                  ],
+                ),
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                color: Color.fromRGBO(253, 255, 241, 1),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  width: 2,
+                  color: Color.fromRGBO(255, 138, 0, 1),
+              ),
+              ),
+
+                      ),],
             ),
-          ),
-        ),
             ),
 
 
