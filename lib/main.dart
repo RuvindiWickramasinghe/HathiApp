72774fe0cb7a77f65744a6520e5674f1a05b1211
Ruvindi Wickramasinghe <146427042+RuvindiWickramasinghe@users.noreply.github.com>
2024-03-05 12:18:02 +0530

    sample code

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      
-      
-     
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Flutter'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+
 
   
 
