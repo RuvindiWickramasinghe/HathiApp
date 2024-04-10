959dfbafa386e9e484abb10de7f833f753ca9066
Binadi Karunathilake <137859884+binadi-karunathilake@users.noreply.github.com>
2024-04-10 22:22:50 +0530

    Update buyer_home.dart

--- lib/buyer_home.dart
+++ lib/buyer_home.dart
@@ -350,7 +350,7 @@ class _HomePageState extends State<HomePage> {
                             children: [
                               Text(
                                   style: TextStyle(fontWeight: FontWeight.bold),
-                                  'Electrical Services'),//Electrical services button
+                                  'Electrical Services'),//Electrical service button
                             ],
                           ),
                         ],
