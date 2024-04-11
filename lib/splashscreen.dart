c5bf51115b9032dc54cf391e73d8f962e1619992
PrabAsha2001 <umiasha79@gmail.com>
2024-04-12 01:29:37 +0530

    prabhasha's commit

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,43 +1,47 @@
-import 'dart:async'; // Importing the async library for using Timer
-
-import 'package:flutter/material.dart'; // Importing Flutter material package
-import 'package:hathi_app/main.dart'; // Importing the main.dart file from the hathi_app package
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:hathi_app/main.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key}); // SplashScreen widget definition
+  const SplashScreen({Key key}) : super(key: key);
 
   @override
-  _SplashScreenState createState() => _SplashScreenState(); // Create state for SplashScreen
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
     // Add a delay of 2 seconds before navigating to the SignInPage
     Timer(
-      const Duration(seconds: 2), // Setting a 2-second delay
-      () => Navigator.pushReplacement( // Navigating to another page after the delay
+      const Duration(seconds: 2),
+      () => Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const hathiApp()), // Navigating to hathiApp widget
+        MaterialPageRoute(builder: (context) => const hathiApp()),
       ),
     );
+    // Call the initTasks function
+    initTasks();
   }
 
-  
+  // Additional function to perform initialization tasks
+  void initTasks() {
+    // Perform your initialization tasks here
+    print('Performing initialization tasks...');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Returning a Scaffold widget
-      body: Center( // Centering the content
+    return Scaffold(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
-              'assets/hathi_logo.png', // Loading an image asset
+              'assets/hathi_logo.png',
               width: MediaQuery.of(context).size.width * 0.8,
               height: MediaQuery.of(context).size.height * 0.8,
-              // You can adjust the width and height based on your preference
             ),
           ],
         ),
