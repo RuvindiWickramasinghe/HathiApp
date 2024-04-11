1b7f7126e1059005305e89ee476e822999ffe155
PrabAsha2001 <umiasha79@gmail.com>
2024-04-12 01:23:37 +0530

    prabhasha's commit

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,13 +1,13 @@
-import 'dart:async';
+import 'dart:async'; // Importing the async library for using Timer
 
-import 'package:flutter/material.dart';
-import 'package:hathi_app/main.dart';
+import 'package:flutter/material.dart'; // Importing Flutter material package
+import 'package:hathi_app/main.dart'; // Importing the main.dart file from the hathi_app package
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({super.key}); // SplashScreen widget definition
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState(); // Create state for SplashScreen
 }
 
 class _SplashScreenState extends State<SplashScreen> {
@@ -17,23 +17,24 @@ class _SplashScreenState extends State<SplashScreen> {
 
     // Add a delay of 2 seconds before navigating to the SignInPage
     Timer(
-      const Duration(seconds: 2),
-      () => Navigator.pushReplacement(
+      const Duration(seconds: 2), // Setting a 2-second delay
+      () => Navigator.pushReplacement( // Navigating to another page after the delay
         context,
-        MaterialPageRoute(builder: (context) => const hathiApp()),
+        MaterialPageRoute(builder: (context) => const hathiApp()), // Navigating to hathiApp widget
       ),
     );
   }
 
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return Scaffold( // Returning a Scaffold widget
+      body: Center( // Centering the content
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
-              'assets/hathi_logo.png', // Replace with your image asset
+              'assets/hathi_logo.png', // Loading an image asset
               width: MediaQuery.of(context).size.width * 0.8,
               height: MediaQuery.of(context).size.height * 0.8,
               // You can adjust the width and height based on your preference
