1c33d1111c6c1018ab502745a6591eea162778fe
Binadi Karunathilake <137859884+binadi-karunathilake@users.noreply.github.com>
2024-04-12 12:27:35 +0530

    Update buyer_home.dart

--- lib/buyer_home.dart
+++ lib/buyer_home.dart
@@ -47,7 +47,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
 
-//this is the menu//
+
 
       drawer: Drawer(
         backgroundColor: const Color.fromRGBO(232, 99, 70, 1),
